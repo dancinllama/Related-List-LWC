83668dfd82d030a299d056599ce84c486eada5f3
James Loghry <james.loghry@edlconsulting.com>
2019-01-07 17:20:52 -0600

    Update relatedList-test.js

--- force-app/main/default/lwc/relatedList/__tests__/relatedList-test.js
+++ force-app/main/default/lwc/relatedList/__tests__/relatedList-test.js
@@ -5,22 +5,5 @@ import { createElement } from 'lwc';
 import RelatedListTest from 'c/relatedList';
 
 describe('relatedList', () => {
-    it('displays expected header text', true);/*() => {
-        const expectedText = 'Hello, Lightning web components';
-
-        // Create an instance of the component under test
-        const element = createElement('c-hello-world', { is: LwcHelloWorld });
-
-        // Add component to DOM (jsdom) so it renders, goes through creation lifecycle
-        document.body.appendChild(element);
-
-        // Find the header element we want to inspect
-        const header = element.shadowRoot.querySelector('h1');
-
-        // Compare the found text with the expected value
-        expect(header.textContent).toBe(expectedText);
-
-        // Available "expect" APIs can be found here: 
-        // https://facebook.github.io/jest/docs/en/expect.html
-    });*/
+    it('displays expected header text', () => {});
 });
